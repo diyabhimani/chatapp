4fbc6605c2c9a4c6b549a4177ab6c7356e56f717
diyabhimani <diyabhimani@gmail.com>
2026-06-20 17:22:38 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ChatApp',
+      title: 'ChatApp1',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
